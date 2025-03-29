ff332446fda45621ca51155ad8f4f6c4595144a3
unknown <SSDD@PNAWSA05LC54423.duoc.acad>
2025-03-29 13:05:51 -0300

    se agregan path

--- src/main/java/com/example/proyecto/controller/Proyecto1Controller.java
+++ src/main/java/com/example/proyecto/controller/Proyecto1Controller.java
@@ -2,7 +2,6 @@ package com.example.proyecto.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -10,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Proyecto1Controller {
 
     @GetMapping("/hola")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public String holaMundo() {
+        return "Holissss Mundo";
+
+    }
+    
+    @GetMapping("/chao")
+    public String otrometodo() {
+        return "adiosito";
+
     }
     
 
